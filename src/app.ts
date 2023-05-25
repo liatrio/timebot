485f83601ed2d85b9eabd7eca0c99dccaefa45ae
Eliot Eikenberry <eliote@liatrio.com>
2023-05-25 17:51:30 +0000

    add health check

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,51 @@ webserver.get("/", (_: any, res: any) => {
   logger.debug("root path response sent");
 });
 
+
+
+webserver.get("/health", async (_: any, res: any) => {
+	const status_checks: any = {};
+  
+	try {
+	  const slack_api_status = await app.client.api.test();
+
+	  if (slack_api_status.ok) {
+		status_checks.slack_api = "OK";
+	  }
+	} catch (e: any) {
+	  status_checks.slack_api = e.message;
+	}
+  
+	try {
+	  const slack_auth_status = await app.client.auth.test();
+
+	  if (slack_auth_status.ok) {
+		status_checks.slack_auth = "OK";
+	  }
+	} catch (e: any) {
+	  status_checks.slack_auth = e.message;
+	}
+  
+	status_checks.slack_websocket_connection = app.receiver.client.badConnection
+	  ? "Connection Failed"
+	  : "OK";
+  
+	for (const i in status_checks) {
+	  if (status_checks[i] !== "OK") {
+		res.status(500).send(status_checks);
+	
+		logger.error("Health check failed", {
+		  status_checks,
+		});
+	
+		return;
+	  }
+	}
+
+	res.send(status_checks);
+	logger.debug("Health check passed");
+  });
+
 var normalizedPath = require("path").join(__dirname, "commands");
 require("fs")
   .readdirSync(normalizedPath)
