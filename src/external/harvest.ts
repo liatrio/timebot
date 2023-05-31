9e76fe85d4262af481ada362f9f0e6ea0d08685f
Eliot Eikenberry <eliote@liatrio.com>
2023-05-31 16:08:19 +0000

    feat(command): projtime command implementation

--- src/external/harvest.ts
+++ src/external/harvest.ts
@@ -2,7 +2,7 @@ function getHarvestRequestOpts(method: string) {
 	return {
 		method: method,
 		headers: {
-			"Authorization": process.env.HARVEST_ACCESS_TOKEN,
+			"Authorization": `Bearer ${process.env.HARVEST_ACCESS_TOKEN}`,
 			"Harvest-Account-Id": process.env.HARVEST_ACCOUNT_ID,
 			"User-Agent": "Liatrio timebot"
 		}
@@ -14,55 +14,55 @@ function buildHarvestUri(endpoint: string) {
 }
 
 export async function getUserTimeEntries(userId: string, from: Date, to: Date) {
-	let requestOpts: any = getHarvestRequestOpts("GET");
-	let endpoint = "time_entries?user_id=" + userId + "&from=" + from + "&to=" + to;
-	let uri = buildHarvestUri(endpoint);
-	let response = await fetch(uri, requestOpts);
+	const requestOpts: any = getHarvestRequestOpts("GET");
+	const endpoint = "time_entries?user_id=" + userId + "&from=" + from + "&to=" + to;
+	const uri = buildHarvestUri(endpoint);
+	const response = await fetch(uri, requestOpts);
 
 	return await response.json();
 }
 
-export async function getProjectTimeEntries(projectId: string, from: Date, to: Date) {
-	let requestOpts: any = getHarvestRequestOpts("GET");
-	let endpoint = "time_entries?project_id=" + projectId + "&from=" + from + "&to=" + to;
-	let uri = buildHarvestUri(endpoint);
-	let response = await fetch(uri, requestOpts);
+export async function getProjectTimeEntries(projectId: string) {
+	const requestOpts: any = getHarvestRequestOpts("GET");
+	const endpoint = "time_entries?project_id=" + projectId;
+	const uri = buildHarvestUri(endpoint);
+	const response = await fetch(uri, requestOpts);
 
 	return await response.json();
 }
 
-export async function getUserList(onlyActive: boolean = false) {
-	let requestOpts: any = getHarvestRequestOpts("GET");
-	let endpoint = onlyActive ? "users?is_active=true" : "users";
-	let uri = buildHarvestUri(endpoint);
-	let response = await fetch(uri, requestOpts);
+export async function getUserList(onlyActive = false) {
+	const requestOpts: any = getHarvestRequestOpts("GET");
+	const endpoint = onlyActive ? "users?is_active=true" : "users";
+	const uri = buildHarvestUri(endpoint);
+	const response = await fetch(uri, requestOpts);
 
 	return await response.json();
 }
 
-export async function getProjectList(onlyActive: boolean = false) {
-	let requestOpts: any = getHarvestRequestOpts("GET");
-	let endpoint = onlyActive ? "projects?is_active=true" : "users";
-	let uri = buildHarvestUri(endpoint);
-	let response = await fetch(uri, requestOpts);
+export async function getProjectList(onlyActive = false) {
+	const requestOpts: any = getHarvestRequestOpts("GET");
+	const endpoint = onlyActive ? "projects?is_active=true" : "projects";
+	const uri = buildHarvestUri(endpoint);
+	const response = await fetch(uri, requestOpts);
 
 	return await response.json();
 }
 
 export async function getUserProjectAssignments(userId: string) {
-	let requestOpts: any = getHarvestRequestOpts("GET");
-	let endpoint = "users/" + userId + "/project_assignments";
-	let uri = buildHarvestUri(endpoint);
-	let response = await fetch(uri, requestOpts);
+	const requestOpts: any = getHarvestRequestOpts("GET");
+	const endpoint = "users/" + userId + "/project_assignments";
+	const uri = buildHarvestUri(endpoint);
+	const response = await fetch(uri, requestOpts);
 
 	return await response.json();
 }
 
-export async function getProjectUserAssignments(projectId: string, onlyActive: boolean = false) {
-	let requestOpts: any = getHarvestRequestOpts("GET");
-	let endpoint = onlyActive ? "projects/" + projectId + "/user_assignments?is_active=true" : "projects/" + projectId + "/user_assignments";
-	let uri = buildHarvestUri(endpoint);
-	let response = await fetch(uri, requestOpts);
+export async function getProjectUserAssignments(projectId: string, onlyActive = false) {
+	const requestOpts: any = getHarvestRequestOpts("GET");
+	const endpoint = onlyActive ? "projects/" + projectId + "/user_assignments?is_active=true" : "projects/" + projectId + "/user_assignments";
+	const uri = buildHarvestUri(endpoint);
+	const response = await fetch(uri, requestOpts);
 
 	return await response.json();
 }
