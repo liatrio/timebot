63d0ed93d43ff31161063bd27a5c0532165ed36d
Eliot Eikenberry <eliote@liatrio.com>
2023-06-16 13:58:48 +0000

    fix(tf): remove depends/fix vault id

--- infra/terraform/vault.tf
+++ infra/terraform/vault.tf
@@ -12,39 +12,23 @@ resource "azurerm_role_assignment" "timebot" {
 resource "azurerm_key_vault_secret" "app_token" {
   name         = "app-token"
   value        = var.app_token
-  key_vault_id = azurerm_key_vault.timebot.id
-
-  depends_on = [
-    azurerm_key_vault_access_policy.terraform_access
-  ]
+  key_vault_id = data.azurerm_key_vault.timebot.id
 }
 
 resource "azurerm_key_vault_secret" "bot_user_token" {
   name         = "bot-user-token"
   value        = var.bot_user_token
-  key_vault_id = azurerm_key_vault.timebot.id
-
-  depends_on = [
-    azurerm_key_vault_access_policy.terraform_access
-  ]
+  key_vault_id = data.azurerm_key_vault.timebot.id
 }
 
 resource "azurerm_key_vault_secret" "harvest_token" {
   name         = "harvest-token"
   value        = var.harvest_token
-  key_vault_id = azurerm_key_vault.timebot.id
-
-  depends_on = [
-    azurerm_key_vault_access_policy.terraform_access
-  ]
+  key_vault_id = data.azurerm_key_vault.timebot.id
 }
 
 resource "azurerm_key_vault_secret" "harvest_id" {
   name         = "harvest-id"
   value        = var.harvest_id
-  key_vault_id = azurerm_key_vault.timebot.id
-
-  depends_on = [
-    azurerm_key_vault_access_policy.terraform_access
-  ]
+  key_vault_id = data.azurerm_key_vault.timebot.id
 }
